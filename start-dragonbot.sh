e49ef2b60c6274959035a656a8ea7670dfd3d0a0
David Radcliffe <dradcliffe@gmail.com>
2019-08-31 13:00:47 -0500

    Add Hubot scripts and bot configuration

---
+++ start-dragonbot.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run Hubot with the Gitter adapter. Set up as documented at
+# https://www.npmjs.com/package/hubot-gitter
+
+# Define settings for the hubot script.
+export PORT=8889;
+#export HUBOT_GITTER2_TOKEN=729015f72a4f1fe84a4f9a026e4e57cbd7d18803;
+export HUBOT_GITTER2_TOKEN=a37136d5e5a07de29598c34e724bdfc30b42e060;
+
+# Temporary variables.
+#export HUBOT_LOG_LEVEL=debug
+
+cd /Users/davidradcliffe/dragonbot;
+# ./bin/hubot -a gitter2 --name dragonbot2 > dragonbot2.log 2>&1 &
+./bin/hubot -a gitter2 --name dragonbot2;
